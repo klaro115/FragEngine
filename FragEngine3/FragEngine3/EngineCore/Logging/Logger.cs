ad26ad96459304539f8df60871d6c83819acda8d
Robert Klares <klaro115@hotmail.com>
2023-11-09 18:56:13 +0100

    Implemented basic log writing

--- FragEngine3/FragEngine3/EngineCore/Logging/Logger.cs
+++ FragEngine3/FragEngine3/EngineCore/Logging/Logger.cs
@@ -9,6 +9,10 @@ namespace FragEngine3.EngineCore
 		public Logger(Engine _engine)
 		{
 			engine = _engine ?? throw new ArgumentNullException(nameof(_engine), "Engine may not be null!");
+
+			applicationPath = Path.GetFullPath(Environment.CurrentDirectory);
+			logDirAbsPath = Path.Combine(applicationPath, LoggerConstants.LOG_FILE_DIR_REL_PATH);
+			logFileAbsPath = Path.Combine(logDirAbsPath, LoggerConstants.MAIN_LOG_FILE_NAME);
 		}
 
 		#endregion
@@ -16,12 +20,81 @@ namespace FragEngine3.EngineCore
 
 		public readonly Engine engine;
 
-		private readonly List<LogEntry> entries = new(256);
-		private readonly int maxEntryCount = 256;
-		//private readonly int writeLogsEveryNEntries = 1;
+		private readonly List<LogEntry> entries = new(64);
+		private readonly int maxEntryCountBeforeClear = 64;
+		private readonly int keepEntryCountAfterClear = 4;
+		private readonly int writeLogsEveryNEntries = 1;
+		private int entriesKeptAfterClear = 0;
+
+		public readonly string applicationPath;
+		public readonly string logDirAbsPath;
+		public readonly string logFileAbsPath;
+
+		private static readonly object lockObj = new();
+
+		#endregion
+		#region Properties
+
+		public bool IsInitialized { get; private set; } = false;
 
 		#endregion
 		#region Methods
+
+		public bool Initialize()
+		{
+			if (IsInitialized) return true;
+
+			// Ensure the log file and its parent directory exist:
+			bool createdNew = false;
+			StreamWriter? writer = null;
+			try
+			{
+				lock(lockObj)
+				{
+					if (!File.Exists(logFileAbsPath))
+					{
+						if (!Directory.Exists(logDirAbsPath))
+						{
+							Directory.CreateDirectory(logDirAbsPath);
+						}
+
+						writer = File.CreateText(logFileAbsPath);
+						createdNew = true;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error! Failed to create log file directory and main log file!\nException type: '{ex.GetType()}'\nException message: '{ex.Message}'");
+				IsInitialized = false;
+				return false;
+			}
+			finally
+			{
+				writer?.Close();
+			}
+
+			// Log the moment the log file was created and written to:
+			if (createdNew)
+			{
+				LogMessage("Log file created.");
+			}
+
+			LogMessage("-----------------------------------------------");
+
+			IsInitialized = true;
+			return true;
+		}
+
+		public void Shutdown()
+		{
+			LogMessage("Logging session ended.");
+			LogMessage("-----------------------------------------------");
+
+			//TODO: Write all pending entries to file!
+
+			IsInitialized = false;
+		}
 
 		public void LogMessage(string _message)
 		{
@@ -29,15 +102,25 @@ namespace FragEngine3.EngineCore
 			LogNewEntry(entry);
 		}
 
-		public void LogError(string _message, int _errorCode = 0, LogEntrySeverity _severity = LogEntrySeverity.Normal)
+		public void LogError(string _message)
+		{
+			LogEntry entry = new(LogEntryType.Error, _message, -1, LogEntrySeverity.Normal);
+			LogNewEntry(entry);
+		}
+		public void LogError(string _message, int _errorCode = -1, LogEntrySeverity _severity = LogEntrySeverity.Normal)
 		{
 			LogEntry entry = new(LogEntryType.Error, _message, _errorCode, _severity);
 			LogNewEntry(entry);
 		}
 
+		public void LogException(string _message, Exception _exception)
+		{
+			LogEntry entry = new(LogEntryType.Error, _message, -1, LogEntrySeverity.Major, _exception);
+			LogNewEntry(entry);
+		}
 		public void LogException(string _message, Exception _exception, LogEntrySeverity _severity = LogEntrySeverity.Normal)
 		{
-			LogEntry entry = new(LogEntryType.Error, _message, 0, _severity, _exception);
+			LogEntry entry = new(LogEntryType.Error, _message, -1, _severity, _exception);
 			LogNewEntry(entry);
 		}
 
@@ -45,29 +128,57 @@ namespace FragEngine3.EngineCore
 		{
 			entries.Add(_entry);
 
-			if (entries.Count > maxEntryCount)
+			// Once enough new 
+			if (entries.Count - keepEntryCountAfterClear >= writeLogsEveryNEntries)
 			{
-				int excessCount = entries.Count - maxEntryCount;
-				for (int i = 0; i < excessCount; ++i)
-				{
-					WriteLogs(entries, 0, excessCount);
-				}
+				int excessCount = Math.Max(entries.Count - maxEntryCountBeforeClear, 0);
+				int firstNewEntryIdx = Math.Clamp(entriesKeptAfterClear - 1, 0, entries.Count);
+				
+				WriteLogs(firstNewEntryIdx, excessCount);
 				entries.RemoveRange(0, excessCount);
 			}
 
+			// Clear list of log entries after a certain number has been logged:
+			if (entries.Count > maxEntryCountBeforeClear)
+			{
+				int excessCount = Math.Max(entries.Count - keepEntryCountAfterClear, 0);
+				entriesKeptAfterClear = Math.Max(entries.Count - excessCount, 0);
+
+				entries.RemoveRange(0, excessCount);
+			}
+
+			// Write the log entry to console for instant user debugging:
 			Console.WriteLine(_entry.ToString());
 		}
 
-		public bool WriteLogs()
+		private bool WriteLogs(int _startIdx, int _count)
 		{
-			//TODO
-			return false;
-		}
+			if (_count == 0) return true;
 
-		public bool WriteLogs(IList<LogEntry> _list, int _startIdx, int _count)
-		{
-			//TODO
-			return false;
+			lock(lockObj)
+			{
+				FileStream? stream = null;
+				TextWriter? writer = null;
+				try
+				{
+					stream = File.Open(logFileAbsPath, FileMode.Append, FileAccess.Write);
+					writer = new StreamWriter(stream);
+
+					int endIdx = Math.Min(_startIdx + _count, entries.Count);
+					for (int i = _startIdx; i < endIdx; ++i)
+					{
+						LogEntry entry = entries[i];
+						writer.WriteLine(entry.ToString());
+					}
+				}
+				finally
+				{
+					writer?.Dispose();
+					stream?.Close();
+				}
+			}
+
+			return true;
 		}
 
 		#endregion
---
+++ FragEngine3/FragEngine3/EngineCore/Logging/LoggerConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FragEngine3.EngineCore.Logging
+{
+	public static class LoggerConstants
+	{
+		#region Constants
+
+		public const string LOG_FILE_DIR_REL_PATH = "logs";
+		public const string MAIN_LOG_FILE_NAME = "main";
+
+		#endregion
+	}
+}
+
