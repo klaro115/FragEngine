0af1d31f8bb153dff8ef0146cb3a5aa73089af32
Robert Desktop <klaro115@hotmail.com>
2024-01-18 23:23:46 +0100

    Added ImageImporter class; Started adding bitmap image importer; Added RawImageData type for CPU-side image data representation
    (Texture import is very WIP, bitmap importer not yet functional)

---
+++ FragEngine3/FragEngine3/Graphics/Resources/Data/RawImageData.cs
@@ -0,0 +1,99 @@
+﻿using FragEngine3.EngineCore;
+using System.Collections;
+using Veldrid;
+
+namespace FragEngine3.Graphics.Resources.Data
+{
+	public sealed class RawImageData : IEnumerable<Color32>
+	{
+		#region Fields
+
+		public PixelFormat pixelFormat = PixelFormat.B8_G8_R8_A8_UNorm_SRgb;
+		public uint width = 0;
+		public uint height = 0;
+		public uint dpi = 0;
+
+		public Color32[] pixelData = [];
+
+		#endregion
+		#region Properties
+
+		public bool IsValid => pixelData != null && PixelCount <= pixelData.Length;
+
+		public uint PixelCount => width * height;
+		public uint DataCount => Math.Min(pixelData != null ? (uint)pixelData.Length : 0u, PixelCount);
+
+		public uint MaxDataByteSize => width * height * Color32.byteSize;
+
+		public Color32 this[uint _pixelIdx]
+		{
+			get => _pixelIdx < pixelData.Length ? pixelData[_pixelIdx] : Color32.TransparentBlack;
+			set
+			{
+				if (_pixelIdx < pixelData.Length) pixelData[_pixelIdx] = value;
+			}
+		}
+		
+		public Color32 this[uint _x, uint _y]
+		{
+			get
+			{
+				uint pixelIdx = _y * width + _x;
+				return pixelIdx < pixelData.Length ? pixelData[pixelIdx] : Color32.TransparentBlack;
+			}
+			set
+			{
+				uint pixelIdx = _y * width + _x;
+				if (pixelIdx < pixelData.Length) pixelData[pixelIdx] = value;
+			}
+		}
+
+		#endregion
+		#region Methods
+
+		/// <summary>
+		/// Reverse row order of the image, therefore mirroring its content vertically.<para/>
+		/// NOTE: This operation is done as in-place as possible, using a small single-row buffer as intermediate storage.
+		/// </summary>
+		/// <returns>True if the image was mirrored vertically, false if the image was invalid.</returns>
+		public bool MirrorVertically()
+		{
+			if (!IsValid)
+			{
+				Logger.Instance?.LogError("Cannot mirror invalid raw image along Y-axis!");
+				return false;
+			}
+
+			uint halfHeight = height / 2;
+
+			Color32[] rowBuffer = new Color32[width];
+			for (uint y = 0; y < halfHeight; ++y)
+			{
+				long srcLineStartIdx = y * width;
+				long dstLineStartIdx = (height - y - 1) * width;
+
+				Array.Copy(pixelData, srcLineStartIdx, rowBuffer, 0, width);				// src => buffer
+				Array.Copy(pixelData, dstLineStartIdx, rowBuffer, srcLineStartIdx, width);  // dst => src
+				rowBuffer.CopyTo(pixelData, dstLineStartIdx);								// buffer => dst
+			}
+
+			return true;
+		}
+
+		public IEnumerator<Color32> GetEnumerator()
+		{
+			for (uint i = 0; i < DataCount; ++i)
+			{
+				yield return pixelData[i];
+			}
+		}
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		public override string ToString()
+		{
+			return $"RawImageData (Resolution: {width}x{height}, Format: {pixelData}, Size: {MaxDataByteSize} bytes)";
+		}
+
+		#endregion
+	}
+}
---
+++ FragEngine3/FragEngine3/Graphics/Resources/Import/BitmapImporter.cs
@@ -0,0 +1,155 @@
+﻿using FragEngine3.EngineCore;
+using FragEngine3.Graphics.Resources.Data;
+
+namespace FragEngine3.Graphics.Resources.Import
+{
+	public static class BitmapImporter
+	{
+		#region Types
+
+		private struct BmpHeader
+		{
+			public ushort bfType;
+			public uint bfSize;
+			public uint bfReserved;
+			public uint bfOffBits;
+		}
+
+		private struct BmpInfoBlock
+		{
+			public uint bfSize;
+			public int biWidth;
+			public int biHeight;
+			public ushort biPlanes;
+			public ushort biBitCount;
+			public uint biCompression;
+			public uint biSizeImage;
+			public int biXPelsPerMeter;
+			public int biYPelsPerMeter;
+			public uint biClrUsed;
+			public uint biClrImportant;
+		}
+
+		#endregion
+		#region Methods
+
+		public static bool ImportImage(Stream _byteStream, out RawImageData? _outRawImage)
+		{
+			// Link with format reference: https://de.wikipedia.org/wiki/Windows_Bitmap
+			if (_byteStream == null)
+			{
+				Logger.Instance?.LogError("Cannot import bitmap image from null stream!");
+				_outRawImage = null;
+				return false;
+			}
+			if (!_byteStream.CanRead)
+			{
+				Logger.Instance?.LogError("Cannot import bitmap image from write-only stream!");
+				_outRawImage = null;
+				return false;
+			}
+
+			_outRawImage = new RawImageData();
+
+			long streamStartPosition = _byteStream.Position;
+			BinaryReader reader = new(_byteStream);
+
+			if (!ReadHeader(reader, out BmpHeader header))
+			{
+				Logger.Instance?.LogError("Failed to parse header of bitmap image!");
+				return false;
+			}
+
+			if (!ReadInfoBlock(reader, out BmpInfoBlock infoBlock))
+			{
+				Logger.Instance?.LogError("Failed to parse info block of bitmap image!");
+				return false;
+			}
+			
+			if (!ReadColorMasks(reader))	//TODO: Conditional!
+			{
+				Logger.Instance?.LogError("Failed to parse color masks of bitmap image!");
+				return false;
+			}
+
+			if (!ReadColorTable(reader))	//TODO: Conditional!
+			{
+				Logger.Instance?.LogError("Failed to parse color table of bitmap image!");
+				return false;
+			}
+
+			// Move to pixel data block:
+			if (_byteStream.CanSeek)
+			{
+				_byteStream.Position = streamStartPosition + header.bfOffBits;
+			}
+
+			//TODO: Parse pixel data.
+
+			return true;
+		}
+
+		private static bool ReadHeader(BinaryReader _reader, out BmpHeader _outHeader)
+		{
+			try
+			{
+				_outHeader = new BmpHeader()
+				{
+					bfType			= _reader.ReadUInt16(),	// ASCII "BM"
+					bfSize			= _reader.ReadUInt32(),	// File size in bytes
+					bfReserved		= _reader.ReadUInt32(),	// Reserved, should be 0
+					bfOffBits		= _reader.ReadUInt32(),	// Byte offset where image data starts, usually 54
+				};
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Logger.Instance?.LogException("Failed to read bitmap image header from byte stream!", ex);
+				_outHeader = default;
+				return false;
+			}
+		}
+		
+		private static bool ReadInfoBlock(BinaryReader _reader, out BmpInfoBlock _outInfoBlock)
+		{
+			try
+			{
+				_outInfoBlock = new BmpInfoBlock()
+				{
+					bfSize			= _reader.ReadUInt32(),
+					biWidth			= _reader.ReadInt32(),
+					biHeight		= _reader.ReadInt32(),
+					biPlanes		= _reader.ReadUInt16(),
+					biBitCount		= _reader.ReadUInt16(),
+					biCompression	= _reader.ReadUInt32(),
+					biSizeImage		= _reader.ReadUInt32(),
+					biXPelsPerMeter	= _reader.ReadInt32(),
+					biYPelsPerMeter	= _reader.ReadInt32(),
+					biClrUsed		= _reader.ReadUInt16(),
+					biClrImportant	= _reader.ReadUInt16(),
+				};
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Logger.Instance?.LogException("Failed to read bitmap image info block from byte stream!", ex);
+				_outInfoBlock = default;
+				return false;
+			}
+		}
+
+		private static bool ReadColorMasks(BinaryReader _reader /* TODO */)
+		{
+			//TODO
+			return false;
+		}
+
+		private static bool ReadColorTable(BinaryReader _reader /* TODO */)
+		{
+			//TODO
+			return false;
+		}
+
+		#endregion
+	}
+}
