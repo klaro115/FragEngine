c987a3d5f2a88dc2ddaaba83c8d0aa3bbf0061de
Robert Desktop <klaro115@hotmail.com>
2024-08-03 23:12:31 +0200

    Rewrote DXC compiler logic using Vortice

--- FragEngine3/FragAssetPipeline/Resources/Shaders/DxcLauncher.cs
+++ FragEngine3/FragAssetPipeline/Resources/Shaders/DxcLauncher.cs
@@ -1,7 +1,7 @@
 ﻿using FragEngine3.Graphics;
-using System.Diagnostics;
 using System.Text;
 using Veldrid;
+using Vortice.Dxc;
 
 namespace FragAssetPipeline.Resources.Shaders;
 
@@ -9,10 +9,10 @@ public static class DxcLauncher
 {
 	#region Types
 
-	public sealed class DxcResult(bool _isSuccess, string? _compiledCode = null)
+	public sealed class DxcResult(bool _isSuccess, byte[]? _compiledShader = null)
 	{
 		public readonly bool isSuccess = _isSuccess;
-		public readonly string compiledCode = _compiledCode ?? string.Empty;
+		public readonly byte[] compiledShader = _compiledShader ?? [];
 	}
 
 	#endregion
@@ -20,28 +20,59 @@ public static class DxcLauncher
 
 	private static string? dxcExeAbsolutePath = null;
 
+	private static readonly DxcCompilerOptions compilerOptionsDXBC = new()
+	{
+		// General:
+		ShaderModel = new DxcShaderModel(SHADER_MODEL_MAJOR_VERSION, SHADER_MODEL_MINOR_VERSION),
+
+		// SPIR-V:
+		GenerateSpirv = false,
+	};
+	private static readonly DxcCompilerOptions compilerOptionsSPIRV = new()
+	{
+		// General:
+		ShaderModel = new DxcShaderModel(SHADER_MODEL_MAJOR_VERSION, SHADER_MODEL_MINOR_VERSION),
+
+		// SPIR-V:
+		GenerateSpirv = true,
+		VkUseDXLayout = true,
+		VkUseDXPositionW = true,
+	};
+
 	#endregion
 	#region Constants
-
-	public const string dxcDirRelativePath = ThirdPartyConstants.toolsFolderRelativePath + "DXC/";
-	public const string dxcExeRelativePath = dxcDirRelativePath + dxcExeName;
-	public const string dxcExeName = "dxc.exe";
 
 	public const int SHADER_MODEL_MAJOR_VERSION = 6;
 	public const int SHADER_MODEL_MINOR_VERSION = 3;
 
-	private const int DXC_COMPILER_TIMEOUT = 5000;
-
 	#endregion
 	#region Methods
 
-	public static DxcResult CompileShaderToDXIL(string _hlslFilePath, ShaderStages _shaderStage, string? _entryPoint, string? _includeFilePath = null)
+	public static DxcResult CompileShaderToDXBC(string _hlslFilePath, ShaderStages _shaderStage, string? _entryPoint)
 	{
+		return CompileShader(_hlslFilePath, _shaderStage, _entryPoint, compilerOptionsDXBC);
+	}
+
+	public static DxcResult CompileShaderToSPIRV(string _hlslFilePath, ShaderStages _shaderStage, string? _entryPoint)
+	{
+		return CompileShader(_hlslFilePath, _shaderStage, _entryPoint, compilerOptionsSPIRV);
+	}
+
+	public static DxcResult CompileShader(string _hlslFilePath, ShaderStages _shaderStage, string? _entryPoint, DxcCompilerOptions _options)
+	{
+		// Input null checks:
 		if (string.IsNullOrEmpty(_hlslFilePath))
 		{
 			Console.WriteLine("Error! File path to HLSL shader code may not be null or empty!");
 			return new(false);
 		}
+		if (_options is null)
+		{
+			Console.WriteLine("Error! Cannot compile shader from HLSL code using null compiler options!");
+			return new(false);
+		}
+
+		// Paramneter value and fallbacks:
 		if (_shaderStage == ShaderStages.None && !TryGetShaderStageFromFileName(_hlslFilePath, out _shaderStage))
 		{
 			Console.WriteLine($"Error! Could not determine shader stage of HLSL shader! File path: '{_hlslFilePath}'");
@@ -57,115 +88,53 @@ public static class DxcLauncher
 			}
 			_hlslFilePath = hlslFileAbsPath;
 		}
-
-		dxcExeAbsolutePath ??= Path.GetFullPath(dxcExeRelativePath);
-
-		// Full command format: '<dxc_path> -T <shader_model> -E <entry_point> -I <include_path> <hlsl_path>'
-		List<string> dxcArgumentList = new(5);
-
-		// Format parameter values:
-		if (!GetShaderModelParameter(_shaderStage, dxcArgumentList))
-		{
-			Console.WriteLine($"Error! Could not find shader model parameter for shader stage '{_shaderStage}'!");
-			return new(false);
-		}
-		if (!GetEntryPointParameter(_entryPoint, _shaderStage, dxcArgumentList))
+		if (!GetEntryPointParameter(ref _entryPoint, _shaderStage) || _entryPoint is null)
 		{
 			Console.WriteLine($"Error! Could not find entry point parameter for shader stage '{_shaderStage}'!");
 			return new(false);
 		}
-		GetIncludePathParameter(_includeFilePath, dxcArgumentList);
-		dxcArgumentList.Add($"\"{_hlslFilePath}\"");
 
-		Process? process = null;
-		StringBuilder outputBuilder = new(2048);
+		// Read HLSL shader code from file:
+		string hlslCode;
 		try
 		{
-			ProcessStartInfo processInfo = new(dxcExeAbsolutePath)
-			{
-				CreateNoWindow = true,
-				UseShellExecute = false,
-				RedirectStandardError = true,
-				RedirectStandardOutput = true,
-				//StandardOutputEncoding = Encoding.UTF8,
-			};
-
-			// Assemble full command and log it:
-			Console.WriteLine("Full command:");
-			Console.Write("$    \"");
-			Console.Write(dxcExeAbsolutePath);
-			Console.Write('\"');
-			foreach (string argument in dxcArgumentList)
-			{
-				processInfo.ArgumentList.Add(argument);
-				Console.Write(' ');
-				Console.Write(argument);
-			}
-			Console.WriteLine();
-
-			// Start DXC executable via commandline:
-			process = new()
-			{
-				StartInfo = processInfo,
-			};
-			process.Exited += (sender, e) => Console.WriteLine("Exited.");
-			if (!process.Start())
-			{
-				Console.WriteLine("Error! Failed to start process for DXIL shader compilation using DXC!");
-				return new(false);
-			}
-
-			// Async listeners for output and errors: (yes, this is messy, but operating DXC via CMD is needlessly hard and this is the only way to get reliable output)
-			ReadErrorsAsync();
-			//var task = Task.Run(async () =>
-			//{
-			//	string outputTxt = await process.StandardOutput.ReadToEndAsync();
-			//	if (!string.IsNullOrEmpty(outputTxt))
-			//	{
-			//		outputBuilder.Append(outputTxt);
-			//	}
-			//});
-
-			// Wait for compilation to finish: (yes, there is a WaitForExit method, but it causes DXC to block quietly and never return, so ugly thread sleep logic it is)
-			while (!process.HasExited)
-			{
-				Thread.Sleep(10);
-			}
-
-			if (process.ExitCode != 0)
-			{
-				ReadErrorsAsync()?.Wait();
-				Thread.Sleep(2000);
-				Console.WriteLine($"Error! HLSL to DXIL shader compilation has failed! Shader: '{_hlslFilePath}' ({_shaderStage})! (Exit code: {process.ExitCode})");
-				return new(false);
-			}
-
-			// Output compiled DXIL shader code:
-			string finalOutput = process.StandardOutput.ReadToEnd();
-			outputBuilder.Append(finalOutput);
-			return new(true, outputBuilder.ToString());
+			hlslCode = File.ReadAllText(_hlslFilePath);
 		}
 		catch (Exception ex)
 		{
-			Console.WriteLine($"Error! An exception was caught while compiling HLSL shader to DXIL!\nShader: '{_hlslFilePath}' ({_shaderStage})\nException: {ex}");
+			Console.WriteLine($"Error! Failed to read HLSL shader file at path '{_hlslFilePath}'!\nException: {ex}");
 			return new(false);
 		}
-		finally
-		{
-			process?.Close();
-			process?.Dispose();
-		}
 
-		Task ReadErrorsAsync()
+		try
 		{
-			return Task.Run(async () =>
+			using var results = DxcCompiler.Compile(DxcShaderStage.Vertex, hlslCode, _entryPoint, compilerOptionsDXBC);
+
+			// Check for errors:
+			using var errorBlob = results.GetOutput(DxcOutKind.Errors);
+			if (errorBlob is not null && errorBlob.BufferSize > 0)
 			{
-				string errorTxt = await process.StandardError.ReadToEndAsync();
-				if (!string.IsNullOrEmpty(errorTxt))
-				{
-					Console.WriteLine($"Error: {errorTxt}");
-				}
-			});
+				string errorTxt = Encoding.UTF8.GetString(errorBlob.AsSpan());
+				Console.WriteLine($"Error! Failed to compile HLSL shader code to DXBC!\nFile path: '{_hlslFilePath}'\nError output: '{errorTxt}'");
+				return new(false);
+			}
+
+			// Get compiled shader:
+			using var shaderBlob = results.GetOutput(DxcOutKind.Object);
+			if (shaderBlob is null || shaderBlob.BufferSize == 0)
+			{
+				Console.WriteLine($"Error! Failed to compile HLSL shader code to DXBC; output was empty!\nFile path: '{_hlslFilePath}'");
+				return new(false);
+			}
+
+			// Return success:
+			byte[] shaderBytes = shaderBlob.AsBytes();
+			return new(true, shaderBytes);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error! Failed to compile HLSL shader to DXBC!\nFile path: '{_hlslFilePath}'\nException: {ex}");
+			return new(false);
 		}
 	}
 
@@ -190,58 +159,14 @@ public static class DxcLauncher
 		return false;
 	}
 
-	private static bool GetShaderModelParameter(ShaderStages _shaderStage, List<string> _argumentList)
+	private static bool GetEntryPointParameter(ref string? _entryPoint, ShaderStages _shaderStage)
 	{
-		const char missingPrefix = '_';
-
-		var stagePrefix = _shaderStage switch
-		{
-			ShaderStages.Vertex => 'v',
-			ShaderStages.Geometry => 'g',
-			ShaderStages.Fragment => 'p',
-			ShaderStages.Compute => 'c',
-			_ => missingPrefix,
-		};
-		if (stagePrefix == missingPrefix)
-		{
-			return false;
-		}
-
-		// Format:	'-T <shader_model> '
-		// Example:	'-T ps_6_3 '
-		_argumentList.Add($"-T {stagePrefix}s_{SHADER_MODEL_MAJOR_VERSION}_{SHADER_MODEL_MINOR_VERSION}");
-		return true;
-	}
-
-	private static bool GetEntryPointParameter(string? _entryPoint, ShaderStages _shaderStage, List<string> _argumentList)
-	{
-		// Format:	'-E <entry_point>'
-		// Example:	'-E Main_Pixel '
 		if (!string.IsNullOrEmpty(_entryPoint))
 		{
-			_argumentList.Add($"-E {_entryPoint}");
 			return true;
 		}
 
-		if (!GraphicsConstants.defaultShaderStageEntryPoints.TryGetValue(_shaderStage, out _entryPoint))
-		{
-			_argumentList.Add($"-E {_entryPoint}");
-			return true;
-		}
-		return false;
-	}
-
-	private static bool GetIncludePathParameter(string? _includeFilePath, List<string> _argumentList)
-	{
-		if (string.IsNullOrEmpty(_includeFilePath))
-		{
-			return false;
-		}
-
-		// Format:	'-I <include_path> '
-		// Example:	'-I "./shaders/lighting.hlsl" '
-		_argumentList.Add($"-I \"{_includeFilePath}\"");
-		return true;
+		return GraphicsConstants.defaultShaderStageEntryPoints.TryGetValue(_shaderStage, out _entryPoint);
 	}
 
 	#endregion
