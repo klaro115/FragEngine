33300585eb1ac7df6ef0344ec5a77e1dac28b717
Robert Desktop <klaro115@hotmail.com>
2024-08-03 22:33:05 +0200

    Switch from DXC exe to Vortice's more serviceable NuGet version
    (Calling DXC from command line via code is a nightmare, as the exe will block, not terminate, or justg not output errors reliably. Vortice handles all of this by directly wrapping the C lib/dll)

--- FragEngine3/FragAssetPipeline/Program.cs
+++ FragEngine3/FragAssetPipeline/Program.cs
@@ -1,13 +1,15 @@
 ﻿using FragAssetPipeline.Resources;
 using FragAssetPipeline.Resources.Shaders;
 using Veldrid;
+using Vortice.Dxc;
 
 Console.WriteLine("### BEGIN ###\n");
 
 string testShaderFilePath = Path.GetFullPath(Path.Combine(ResourceConstants.coreFolderRelativePath, "shaders/Basic_VS.hlsl"));
-const ShaderStages testShaderStage = ShaderStages.Vertex;
+//const ShaderStages testShaderStage = ShaderStages.Vertex;
 const string testShaderEntryPoint = "Main_Vertex";
 
+/*
 var result = DxcLauncher.CompileShaderToDXIL(testShaderFilePath, testShaderStage, testShaderEntryPoint);
 
 Console.WriteLine($"Shader compilation: {(result.isSuccess ? "SUCCESS" : "FAILURE")}");
@@ -17,6 +19,51 @@ if (result.isSuccess)
 	Console.WriteLine();
 	Console.WriteLine("### DXIL Code: ###");
 	Console.WriteLine(result.compiledCode);
+}
+*/
+string hlslCode = File.ReadAllText(testShaderFilePath);
+
+DxcCompilerOptions options = new()
+{
+	ShaderModel = DxcShaderModel.Model6_7,
+	
+	// SPIR-V:
+	GenerateSpirv = false,
+	VkUseDXLayout = true,
+	VkUseDXPositionW = true,
+};
+
+using var results = DxcCompiler.Compile(DxcShaderStage.Vertex, hlslCode, testShaderEntryPoint, options);
+for (int i = 0; i < results.NumOutputs; i++)
+{
+	var kind = results.GetOutputByIndex(i);
+	switch (kind)
+	{
+		case DxcOutKind.Object:
+			{
+				using var blob = results.GetOutput(kind);
+				if (blob.BufferSize > 0)
+				{
+					Console.WriteLine("Success.");
+
+					byte[] bytes = blob.AsBytes();
+					string output = System.Text.Encoding.UTF8.GetString(bytes);
+					Console.WriteLine(output);
+				}
+			}
+			break;
+		case DxcOutKind.Errors:
+			{
+				using var blob = results.GetOutput(kind);
+				if (blob.BufferSize != 0)
+				{
+					Console.WriteLine("Error.");
+				}
+			}
+			break;
+		default:
+			break;
+	}
 }
 
 Console.WriteLine("\n#### END ####");
