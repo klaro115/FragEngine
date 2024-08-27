a33a088e0200f781115aec12ef2de532c8e92d88
Robert Desktop <klaro115@hotmail.com>
2024-08-27 21:52:13 +0200

    Refactored bone animation VC include code; Fixed animated variant define name; Added bone-animated variants for default vertex shader

--- data/core/shaders/DefaultSurface_refactored_VS.hlsl
+++ data/core/shaders/DefaultSurface_refactored_VS.hlsl
@@ -59,4 +59,48 @@ void Main_Vertex_Ext(
 }
 #endif //VARIANT_EXTENDED
 
+// ANIMATED VARIANTS:
+
+#ifdef VARIANT_ANIMATED
+void Main_Vertex_Anim(
+    in VertexInput_Basic inputBasic,
+    in VertexInput_BoneWeights inputAnim,
+    out VertexOutput_Basic outputBasic)
+{
+    float4x4 mtxLocal2Clip = mul(mtxWorld2Clip, mtxLocal2World);
+    float3 viewDir = worldPosition - cameraPosition.xyz;
+
+    ApplyBoneAnimation(inputBasic, inputAnim);
+
+    outputBasic.position = mul(mtxLocal2Clip, float4(inputBasic.position, 1));
+    outputBasic.worldPosition = mul(mtxLocal2World, float4(inputBasic.position, 1)).xyz;
+    outputBasic.normal = normalize(mul(mtxLocal2World, float4(inputBasic.normal, 0)).xyz);
+    outputBasic.uv = inputBasic.uv;
+}
+#endif //VARIANT_ANIMATED
+
+#if defined(VARIANT_EXTENDED) && defined(VARIANT_ANIMATED)
+void Main_Vertex_Ext_Anim(
+    in VertexInput_Basic inputBasic,
+    in VertexInput_Extended inputExt,
+    in VertexInput_BoneWeights inputAnim,
+    out VertexOutput_Basic outputBasic,
+    out VertexOutput_Extended outputExt)
+{
+    float4x4 mtxLocal2Clip = mul(mtxWorld2Clip, mtxLocal2World);
+    float3 viewDir = worldPosition - cameraPosition.xyz;
+
+    ApplyBoneAnimation_Ext(inputBasic, inputExt, inputAnim);
+
+    outputBasic.position = mul(mtxLocal2Clip, float4(inputBasic.position, 1));
+    outputBasic.worldPosition = mul(mtxLocal2World, float4(inputBasic.position, 1)).xyz;
+    outputBasic.normal = normalize(mul(mtxLocal2World, float4(inputBasic.normal, 0)).xyz);
+    outputBasic.uv = inputBasic.uv;
+
+    outputExt.tangent = normalize(mul(mtxLocal2World, float4(inputExt.tangent, 0)).xyz);
+    outputExt.binormal = cross(outputBasic.normal, outputExt.tangent);
+    outputExt.uv2 = inputExt.uv2;
+}
+#endif //VARIANT_EXTENDED && VARIANT_ANIMATED
+
 //</FNC>
--- data/core/shaders/includes/VertexData/VertexInput.hlsl
+++ data/core/shaders/includes/VertexData/VertexInput.hlsl
@@ -17,7 +17,7 @@ struct VertexInput_Extended
     float3 tangent : NORMAL1;
     float2 uv2 : TEXCOORD1;
 };
-#endif
+#endif //VARIANT_EXTENDED
 
 #ifdef VARIANT_BLENDSHAPES
 struct VertexInput_BlendShapes
@@ -28,9 +28,9 @@ struct VertexInput_BlendShapes
 // ^Note: Per-index blend shape offsets are provided as a separate vertex buffer, of element type float3.
 // This will be a single contiguous array of all position offsets across all vertices and blend groups.
 // The blend shape offsets vertex buffer is bound after all per-vertex/surface vertex buffers.
-#endif
+#endif //VARIANT_BLENDSHAPES
 
-#ifdef VARIANT_ANIMATION
+#ifdef VARIANT_ANIMATED
 struct VertexInput_BoneWeights
 {
 	uint4 indices : BLENDINDICES1;
@@ -39,7 +39,7 @@ struct VertexInput_BoneWeights
 // ^Note: Per-bone transformation matrices are provided as a separate vertex buffer, of element type float4x4.
 // This will be a single contiguous array of marices of all bones, in the root object's model space. The bone
 // matrix vertex buffer is bound after all per-vertex/surface vertex buffers.
-#endif
+#endif //VARIANT_ANIMATED
 
 //</RES>
 #endif //__HAS_VERTEX_INPUT__
