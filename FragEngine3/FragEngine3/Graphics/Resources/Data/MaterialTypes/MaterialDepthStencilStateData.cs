bfa378de012a221b6e761ac209de0a3b2b459eae
Roby Desktop <klaro115@hotmail.com>
2025-04-06 22:42:21 +0200

    Split rasterizer and depth-stencil state data into their own files

---
+++ FragEngine3/FragEngine3/Graphics/Resources/Data/MaterialTypes/MaterialDepthStencilStateData.cs
@@ -0,0 +1,23 @@
+﻿namespace FragEngine3.Graphics.Resources.Data.MaterialTypes;
+
+[Serializable]
+public sealed class MaterialDepthStencilStateData
+{
+	#region Properties
+
+	// DEPTH:
+
+	public bool EnableDepthTest { get; init; } = true;
+	public bool EnableDepthWrite { get; init; } = true;
+
+	// STENCIL:
+
+	public bool EnableStencil { get; init; } = false;
+	public MaterialStencilBehaviourData? StencilFront { get; init; } = null;
+	public MaterialStencilBehaviourData? StencilBack { get; init; } = null;
+	public byte StencilReadMask { get; init; } = 0;
+	public byte StencilWriteMask { get; init; } = 0;
+	public uint StencilReferenceValue { get; init; } = 0u;
+
+	#endregion
+}
---
+++ FragEngine3/FragEngine3/Graphics/Resources/Data/MaterialTypes/MaterialRasterizerStateData.cs
@@ -0,0 +1,19 @@
+﻿namespace FragEngine3.Graphics.Resources.Data.MaterialTypes;
+
+[Serializable]
+public sealed class MaterialRasterizerStateData
+{
+	#region Properties
+
+	/// <summary>
+	/// Whether the rasterizer should perform culling of triangles if viewed from the wrong side.
+	/// </summary>
+	public bool EnableCulling { get; init; } = true;
+
+	/// <summary>
+	/// Whether to cull triangles in a clockwise sense. If false, culling uses counter-clockwise sense instead.
+	/// </summary>
+	public bool CullClockwise { get; init; } = false;
+
+	#endregion
+}
